1263d8865974eb72066f0caa57af5ffabcd57194
Maryvkth <48543915+Maryvkth@users.noreply.github.com>
2019-11-18 06:55:37 +0800

    Add files via upload

---
+++ showSignatureDish.php
@@ -0,0 +1,87 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "cafedb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Daily Signature Dish</title>
+<link href="style.css" rel="stylesheet"/>
+<link href="sig.css" rel="stylesheet"/>
+</head>
+
+<nav class="nav">
+		<ul>
+			<li><a href="showtable.php">TABLE SUMMARY</a></li>
+			<li><a href="showSignatureDish.php">SIGNATURE FOOD</a></li>
+		</ul>
+</nav>
+<body>
+<br/>
+	<div class="cc">
+      <div class="">
+	  
+	  <br/>
+	  <br/>
+        <h1 align = "center"> \^O^/ Signature Dish \^O^/ </h1>
+		<br/>
+		<br/>
+		
+		<h1 align = "center"><?php
+		$maxnum=0;
+		$max=' ';
+		$sql = mysqli_query($conn,"SELECT foodcode,dishname,MAX(qty) FROM paymenttable");
+		if(mysqli_num_rows($sql)>0)
+		{
+			while($row=mysqli_fetch_array($sql))
+			{	
+	?><img id="cro" src="cro.png" height="80" width="80"/>
+<?php
+		echo $row["dishname"];
+						?><br/><?php
+			$geti="SELECT img from menutable where foodcode=$row[foodcode]";
+			
+				$ima=mysqli_query($conn,$geti);
+				if(mysqli_num_rows($ima)>0)
+				{
+					while($row=mysqli_fetch_array($ima))
+					{
+						?>
+	<img src="<?php echo $row["img"];?>" height="400" width="400"/>
+						<?php	
+					}
+				}
+					
+			}
+		}
+		
+				
+		
+		
+		
+		/*
+		 while($rere = $sql->fetch_assoc()){
+			 if($rere['qty']>$maxnum){
+				 $max=$rere['dishname'];
+				 $maxnum=$max;
+			 }
+		} 
+		echo $max;
+		while($rere=$sql->fetch_assoc())
+		{
+			if($max==$rere['dishname'])
+			{
+				
+			}
+		}*/
+		?></div></div>
+</body>
+</html>
