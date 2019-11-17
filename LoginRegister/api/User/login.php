b4c43c0904c69e7aa5cca98673d8b57b6663cdef
lawwwwww <48543658+lawwwwww@users.noreply.github.com>
2019-11-18 04:03:37 +0800

    pass value

---
+++ LoginRegister/api/User/login.php
@@ -0,0 +1,40 @@
+<?php
+// include database and object files
+include_once '../config/database.php';
+include_once '../objects/user.php';
+ 
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+ 
+// prepare user object
+$user = new User($db);
+// set ID property of user to be edited
+$user->email = isset($_GET['email']) ? $_GET['email'] : die();
+$user->password = base64_encode(isset($_GET['password']) ? $_GET['password'] : die());
+// read the details of user to be edited
+$stmt = $user->login();
+if($stmt->rowCount() > 0){
+    // get retrieved row
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    // create array
+    $user_arr=array(
+        "status" => true,
+        "message" => "Successfully Login!",
+        "empid" => $row['empid'],
+        "email" => $row['email'],
+		print "<a href='http://localhost/dpp2/cook.php?action=addemp&value=".$row['empid']."'>Success! Go to Tables</a>",
+		
+		
+    );
+	
+}
+else{
+    $user_arr=array(
+        "status" => false,
+        "message" => "Invalid email or Password!",
+    );
+}
+// make it json format
+print_r(json_encode($user_arr));
+?>
