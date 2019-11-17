84ef604226d6209ec10d0b35679c5011ded4c1fd
lawwwwww <48543658+lawwwwww@users.noreply.github.com>
2019-11-17 21:49:49 +0800

    Add files via upload

---
+++ navadmin.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+    <title>Cafe</title>
+    <!--meta tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="Cafe">
+	<link href="styleadmin.css" rel="stylesheet"/>
+    </head>
+	
+
+<nav class="nav">
+		<ul>
+			<li><a href="menuSP.php">MANAGE MENU</a></li>
+			<!--<li class="dropdown">
+				<a href="category.html" class="dropbtn">SALES REPORT</a>
+					<div class="dropdown-content">
+					</div>
+			</li>-->
+			
+			<li><a href="tbSP.php">MANAGE TABLE</a></li>
+			<li><a href="empSP.php">MANAGE EMPLOYEE</a></li>
+			<li><a href="payment1.php">SEARCH PAYMENT</a></li>
+		</ul>
+</nav>
+</html>
