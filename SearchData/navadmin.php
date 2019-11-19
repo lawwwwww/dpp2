30155f8ea14cb6f5176a743a55101da54770fd48
lawwwwww <48543658+lawwwwww@users.noreply.github.com>
2019-11-19 20:45:21 +0800

    main page for admin

---
+++ SearchData/navadmin.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+    <title>Cafe</title>
+    <!--meta tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="Cafe">
+	<link href="styleadmin.css" rel="stylesheet"/>
+    <script src="dropdownmenu.js"></script>
+	</head>
+	
+<nav class="nav">
+		<ul>
+			<li><a href="mainadmin.php">HOME</a></li>
+			<li class="dropdown">
+				<a class="dropbtn" onmouseover="showmanage()">MANAGE</a>
+				<div class="dropdown-content">
+				<p id ="manage"></p>
+				</div>
+				</li>
+			
+			<li class="dropdown">
+				<a class="dropbtn" onmouseover="showpayment()">PAYMENT</a>
+				<div class="dropdown-content">
+				<p id="about"></p></div>
+			</li>
+	
+			<li><a href="http://localhost/dpp2/LoginRegister/">LOGOUT</a></li>
+		</ul>
+</nav>
+</html>
