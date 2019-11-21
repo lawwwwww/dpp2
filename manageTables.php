e4705b0f8bbc7b9ddfdeeb4a5c774b2486478b8d
Maryvkth <48543915+Maryvkth@users.noreply.github.com>
2019-11-21 15:41:11 +0800

    Update manageTables.php

--- manageTables.php
+++ manageTables.php
@@ -38,8 +38,7 @@
 <body>
 	<div class="container">
       <div class="">
-        <h1>Table Records</h1>   <!--change here oso-->
-        <div class="col-sm-8">
+        <h1>Table Records</h1>   
 		<div class="well clearfix">
 			<div class="pull-right"><button type="button" class="btn btn-xs btn-primary" id="command-add" data-row-id="0">
 			<span class="glyphicon glyphicon-plus"></span> Record</button></div></div>
