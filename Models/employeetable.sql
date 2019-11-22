435013b06c39a8180834c185b2b1b12fecea9d21
0xvader <alexhaide55@hotmail.com>
2019-11-22 08:56:53 +0800

    Update employeetable.sql

--- Models/employeetable.sql
+++ Models/employeetable.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 17, 2019 at 02:42 PM
+-- Generation Time: Nov 22, 2019 at 01:55 AM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.11
 
@@ -45,9 +45,12 @@ CREATE TABLE `employeetable` (
 --
 
 INSERT INTO `employeetable` (`empid`, `name`, `address`, `contactinfo`, `role`, `gender`, `email`, `password`, `hiredate`) VALUES
-(10, 'Rob', 'Subang Jaya', 123322932, 'Staff', 'Male', 'rob@gmail.com', 'MTIzNDU2Nzg5', '2019-11-17 10:50:05'),
-(11, 'Amy', 'Parksonville', 174448932, 'Admin', 'Female', 'abc@gmail.com', 'MTIzNDU2Nzg5', '2019-11-17 10:45:36'),
-(12, 'Abaca', 'Johor', 130939824, 'Admin', 'Female', 'abaca10@gmail.com', 'MTIzNDU2Nzg5', '2019-11-17 10:54:17');
+(26, 'Rob', 'Parkville', 1298124329, 'Admin', 'Male', 'rob@gmail.com', '123456789', '2019-11-23 00:00:00'),
+(27, 'Bob', 'Texas', 111222983, 'Staff', 'Male', 'bob@gmail.com', '12345', '2019-11-14 00:00:00'),
+(28, 'Abaca', 'Sarawak', 132823127, 'Admin', 'Female', 'abaca@gmail.com', '12345', '2019-11-06 00:00:00'),
+(29, 'Qwerty', 'Qwertyland', 180793409, 'Admin', 'Female', 'qwerty@gmail.com', 'qwerty', '2019-07-24 00:00:00'),
+(30, 'test', 'testland', 10127471, 'Staff', 'Female', 'test@gmail.com', '12345', '0000-00-00 00:00:00'),
+(31, 'John', 'Kuala Lumpur', 173109721, 'Staff', 'Male', 'john@gmail.com', 'johnjohn', '2019-11-10 00:00:00');
 
 --
 -- Indexes for dumped tables
@@ -67,7 +70,7 @@ ALTER TABLE `employeetable`
 -- AUTO_INCREMENT for table `employeetable`
 --
 ALTER TABLE `employeetable`
-  MODIFY `empid` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `empid` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
